5d628ba461faa3b8cf4f447eafb75fa7783ff287
Leonard Martin <116867789+lmartin2422@users.noreply.github.com>
2023-04-28 02:19:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+iimport java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,13 +7,14 @@ public class Main {
         int reversed = 0;
         while (num != 0) {
 
-            int remainder = num % 10;
+            int remainder = num % 10;  // assigns the remainder of the num/10 to the remainder variable
             System.out.println("Remainder: " +remainder);
 
-            reversed = reversed * 10 + remainder;
+            reversed = reversed * 10 + remainder;  // adds the remainder from above to newly stored reverse value
             System.out.println("Reversed: " + reversed);
 
-            num /= 10;
+            // same as num = num/10
+            num /= 10;  // the initial num value is constantly updated reflecting the above divisions
             System.out.println("num: " +num);
         }
         System.out.println("Reversed Number: " + reversed);
